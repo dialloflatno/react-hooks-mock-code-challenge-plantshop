ee3f37e742dc1927957800c7edb2bf0a4e985f70
dialloflatno <brahimdiallo23@gmail.com>
2021-11-10 12:00:50 -0500

    Mock Challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
+const plantUrl = "http://localhost:4000/plants";
+
 
 function App() {
+  const [plantList, setPlantList] = useState([])
+
+  useEffect(() => {
+    fetch(plantUrl)
+      .then(Response => Response.json())
+      .then(data => setPlantList(data))
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage listPlants = {plantList}/>
     </div>
   );
 }
--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,15 +1,26 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ listPlants }) {
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newPlant = []
+    // const newPlantList = [...listPlants, newPlant]
+  }
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" onChange={handleChange} />
+        <input type="text" name="image" placeholder="Image URL" onChange={handleChange} />
+        <input type="number" name="price" step="0.01" placeholder="Price" onChange={handleChange} />
         <button type="submit">Add Plant</button>
+        
       </form>
+      
     </div>
   );
 }
